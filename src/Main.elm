0379a7019492123f62b19ff24fea93a656a6a9c8
Alexander Nenninger <alexander.nenninger@fu-berlin.de>
2021-11-20 13:44:05 +0100

    Add background

--- src/Main.elm
+++ src/Main.elm
@@ -1,44 +1,96 @@
 module Main exposing (..)
 
-import Browser exposing (element)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
-import Html exposing (Html, a)
+import Html exposing (Html, col, main_, menu)
 
 
 main : Html msg
 main =
-    Element.layout []
-        top_bar
+    Element.layout
+        [ width fill
+        , height fill
+        , inFront top_bar
+        , Background.image "assets/background_space_gray.png"
+        ]
+    <|
+        column
+            [ width fill, height fill ]
+            [ top_bar
+            , row [ width fill, height fill ]
+                [ left_panel
+                , main_panel
+                , right_panel
+                ]
+            ]
 
 
 
--- Top Bar
+-- Content
 
 
 top_bar : Element msg
 top_bar =
     row
-        [ screen_width
-        , Background.color (Element.rgb255 51 122 183)
+        -- Style
+        [ width fill
+        , padding 20
+        , spacing 20
+        , Background.color (Element.rgb255 0 0 0)
         ]
-        [ column
-            [ alignLeft
-            , top_bar_padding
-            ]
-            [ el top_bar_format (text "Hello!") ]
-        , column
-            [ alignRight
-            , top_bar_padding
-            ]
-            [ Element.link []
-                { url = "https://github.com/AlexanderNenninger"
-                , label = text "GitHub"
-                }
-            ]
+        -- Content
+        [ el title_format (text "Alexander Nenninger")
+        , link
+            link_format
+            { url = "https://github.com/AlexanderNenninger"
+            , label = text "GitHub"
+            }
+        , link
+            link_format
+            { url = "https://freightdatanow.de/"
+            , label = text "Freight:Data:now"
+            }
         ]
+
+
+left_panel : Element msg -- Have some content in the ccenter of the screen
+left_panel =
+    column
+        [ height fill
+        , width <| fillPortion 1
+        , height fill
+        , Background.color <| rgba255 255 2555 255 0.5
+        ]
+        [ none ]
+
+
+right_panel : Element msg
+right_panel =
+    column
+        [ height fill
+        , width <| fillPortion 1
+        , height fill
+        , Background.color <| rgba255 255 2555 255 0.5
+        ]
+        [ none ]
+
+
+main_panel : Element msg
+main_panel =
+    column
+        [ height fill
+        , width <| fillPortion 4
+        , Background.color <| rgba255 255 255 255 0.5
+        , padding 20
+        ]
+        [ el heading_format <| text ""
+        ]
+
+
+
+-- Top Bar
 
 
 top_bar_padding : Attribute msg
@@ -56,13 +108,47 @@ screen_width =
     Element.width fill
 
 
-top_bar_format : List (Attribute msg)
-top_bar_format =
-    [ Font.center
-    , Font.size 30
-    , h1
-    , Font.medium
+total_height : Attribute msg
+total_height =
+    Element.height fill
+
+
+title_format : List (Attribute msg)
+title_format =
+    [ alignLeft
+    , Font.alignLeft
+    , Font.size 20
+    , font_heavy
+    , Font.bold
     , Font.color (rgb255 255 255 255)
+    ]
+
+
+link_format : List (Attribute msg)
+link_format =
+    [ alignRight
+    , Font.center
+    , Font.size 20
+    , font_regular
+    , Font.color (rgb255 255 255 255)
+    ]
+
+
+heading_format : List (Attribute msg)
+heading_format =
+    [ Font.alignLeft
+    , Font.size 18
+    , font_light
+    , Font.color (rgb255 25 25 25)
+    ]
+
+
+text_format : List (Attribute msg)
+text_format =
+    [ Font.alignLeft
+    , Font.size 12
+    , font_regular
+    , Font.color (rgb255 25 25 25)
     ]
 
 
@@ -70,9 +156,34 @@ top_bar_format =
 -- Type Faces
 
 
-h1 : Attribute msg
-h1 =
+font_heavy : Attribute msg
+font_heavy =
     Font.family
-        [ Font.typeface "Helvetica"
-        , Font.sansSerif
+        [ Font.external
+            { url = "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@700&display=swap"
+            , name = "IBM Plex Mono Heavy"
+            }
+        , Font.monospace
+        ]
+
+
+font_light : Attribute msg
+font_light =
+    Font.family
+        [ Font.external
+            { url = "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300&display=swap"
+            , name = "IBM Plex Mono Light"
+            }
+        , Font.monospace
+        ]
+
+
+font_regular : Attribute msg
+font_regular =
+    Font.family
+        [ Font.external
+            { url = "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400&display=swap"
+            , name = "IBM Plex Mono"
+            }
+        , Font.monospace
         ]
